177309dda054a3ad66474393a014c275f3fcc19c
Morion <t-marat@yandex.ru>
2018-12-19 01:14:48 +0500

    Yandex.Metrika + Google.Analytics

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+        <?php
+            include $_SERVER ['DOCUMENT_ROOT'].'yandexMetrika.php';
+            include $_SERVER ['DOCUMENT_ROOT'].'googleAnalytics.php';
+        ?>
+        
         <title>Yelton</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta charset="UTF-8">
